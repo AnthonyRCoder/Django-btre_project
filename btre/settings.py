dd4defa6ab01a3dc3149f310dd67904444012cb6
rozar <rozariorohanwork@gmail.com>
2022-02-15 11:58:24 +0000

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -150,6 +150,11 @@ MESSAGE_TAGS = {
 # EMAIL_USE_TLS=True
 
 
+try:
+    from .local_settings import *
+except ImportError:
+    pass    
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
